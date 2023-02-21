a3fa08de3ddce58958d0303edee0ca32b4e37dda
imjamnotjelly <jonahmitre@gmail.com>
2023-02-21 11:50:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ def main():
     while True:
         directory = input("Please paste the path of the directory that contains the desired exe: ")
 
+        # checks if directory exists
         print("Confirming directory... ")
         if not os.path.isdir(directory):
             print("Please enter a valid directory. ")
             continue
 
+        # checks if any executables are present
         print("Checking for executables... ")
         os.chdir(directory)
         executables = glob.glob("*.exe")
@@ -22,10 +24,13 @@ def main():
             print("The directory you entered does not contain any executables.")
             continue
 
-        break
+        break  # basically replicates a do/while loop in py
 
-    print(f"\nFound {len(executables)} executable{make_plural(len(executables) != 1)} in {directory}:")
+    # prints array of .exe files
+    print(f"\nFound {len(executables)} executable{make_plural(len(executables) > 1)} in {directory}:")
     print("\n".join("- " + i for i in executables) + "\n")
+
+    # requests user selection of exe
     if len(executables) > 1:
         while True:
             executable = input("Select an executable from the list above: ")
@@ -37,8 +42,9 @@ def main():
 
             print("Please enter a valid executable.")
     else:
-        executable = executables[0]
+        executable = executables[0]  # automatically selects the only executable if only one is present
 
+    # creates temporary batch file
     with open(f"bypass.bat", "w") as f:
         f.write(
             f"""
@@ -46,8 +52,10 @@ def main():
         start {executable[:-4]}
         """)
 
+    # executes and deletes aforementioned batch file
     subprocess.call(["bypass.bat"])
     os.remove("bypass.bat")
+
 
 if __name__ == "__main__":
     main()
